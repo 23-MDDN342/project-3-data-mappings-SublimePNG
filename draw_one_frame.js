9fa10562c74cbf08ef568241c2f990fe7d5833e8
SublimePNG <80139430+SublimePNG@users.noreply.github.com>
2023-06-03 16:13:51 +1200

    FINAL

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,37 +1,92 @@
+var x=300;
+var y=300;
+var a=100;
+var b=100;
+
 function draw_one_frame(cur_frac) {
-  // note: to clear the screen draw a rectangle
-  // that is width x height - like this
-  noStroke();
-  fill(10);
-  rect(0, 0, width, height);
-
-  // note: all shape sizes, line widths, etc. should be a
-  // function of width and height
-  let rect_width = height / 10.0;
-  let half_width = rect_width / 2;
-
-  // note: animation should progress depending on the 
-  // value of cur_frac which goes from 0 to 1, and it
-  // should loop seamlessly
-  let cur_x = map(cur_frac, 0, 1, 0, width) - half_width;
-
-  fill(200);
-  // draw the rect moving across the screen
-  rect(cur_x, 0, rect_width, height);
-  // a second version (offscreen) for when it loops back around
-  rect(cur_x+width, 0, rect_width, height);
-  rect(cur_x-width, 0, rect_width, height);
-
-  // note: you can draw optional things depending on "debugView"
-  if (debugView) {
-    // we'll draw our "keyframes"
-    noFill();
-    stroke(255, 0, 0);
-    strokeWeight(height/100);
-    // here we "plug in" the values when cur_frac is 0
-    rect(-half_width, 0, rect_width, height);
-    rect( width - half_width, 0, rect_width, height);
-    rect(-width - half_width, 0, rect_width, height);
+  
+////////////////////////////////////  SETUP  //////////////////////////////////////////////////////
+// draws a hexagon
+function drawHexagon(x, y, radius) {
+  beginShape();
+  for (let i = 0; i < 6; i++) {
+    let angle = TWO_PI / 6 * i;
+    let px = x + cos(angle) * radius;
+    let py = y + sin(angle) * radius;
+    vertex(px, py);
   }
+  endShape(CLOSE);
 }
 
+// primary and secondary colurs of the noise
+let primaryLight = color("#00f2ff")
+let secondaryLight = color ("#FF0059")
+
+
+///////////////////////////////////  PIXEL GRID  /////////////////////////////////////////////////
+
+// pixel settings
+let pixelSize =width / 10
+let pixelSpacing =width / 100
+
+//draws the grid of pixels
+for(let x = 0; x < width / pixelSpacing; x++){
+    for(let y = 0; y < height / pixelSpacing; y++){ 
+ 
+  // noisy colour for background pixels
+  noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, cur_frac/2, "colour",0.2 ,1, height/2)
+  noisyColour = lerpColor(primaryLight,secondaryLight,noiseColour)
+  
+  noStroke();
+  fill(noisyColour)
+  rect(pixelSpacing*x,pixelSpacing*y, pixelSize);
+}
+}
+
+//////////////////////////////////  HEXAGON GRID  ////////////////////////////////////////////////
+// hexgrid settings
+let hexSize = width / 20
+let hexSizeSmall = width / 23
+
+//let hexSize = width * 70
+//let hexSizeSmall = width * 62
+
+let x_spacing = width / 6
+let y_spacing = width / 20.7
+
+//let x_spacing = 220
+//let y_spacing = 64
+
+// draws the grid of hexagons
+for(let x = 0; x -1 < width / x_spacing; x++){
+    for(let y = 0; y -1 < height / y_spacing; y++){
+      let xPos = x_spacing * x;
+      let yPos = y_spacing * y;
+     // adds an offset to every second row
+      if (y % 2 == 1) { 
+        xPos += x_spacing / 2;
+      }
+
+      // noisycolour for hex edges
+      noiseColour2 = getNoiseValue (xPos*x,yPos*y, cur_frac/2, "colour",0 ,1, height/2)
+      noisyColour2 = lerpColor(primaryLight,secondaryLight,noiseColour)
+      // smaller hex colour
+      let hexColourSmall = color("#0f0f0f")
+
+     // edge lighting on large hexagons
+      fill(noisyColour2)
+      stroke(0)
+      drawHexagon(xPos, yPos, hexSize);
+
+     // dark overlay for large hexagons to dim colour
+      fill(15, 15, 15, 220)
+      stroke(0)
+      drawHexagon(xPos, yPos, hexSize);
+
+    // smaller inner hexagons
+      fill(hexColourSmall)
+      noStroke()
+      drawHexagon(xPos, yPos, hexSizeSmall);
+    }
+  }
+}
